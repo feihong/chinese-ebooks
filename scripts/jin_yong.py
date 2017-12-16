3fd6c4d4b236e85c54946b1e1f362d2395ce1664
Feihong Hsu <feihong.hsu@gmail.com>
2017-12-15 22:43:33 -0600

    Add script for Jin Yong novels (incomplete)

---
+++ scripts/jin_yong.py
@@ -0,0 +1,23 @@
+"""
+All Jin Yong novels:
+
+https://en.wikipedia.org/wiki/Jin_Yong#Novels
+
+"""
+from shu.kanunu import make_ebook
+
+
+BOOKS = """
+http://www.kanunu8.com/book/4600/index.html 书剑恩仇录 shu jian en chou lu
+http://www.kanunu8.com/wuxia/201102/1622.html 碧血剑 bi xue jian
+"""
+BOOKS = [line.split(' ', 2) for line in BOOKS.strip().splitlines()]
+
+
+for url, title, output_file in BOOKS:
+    make_ebook(
+        index_url=url,
+        title=title,
+        author='金庸',
+        output_file=f'books/{output_file}'
+    )
