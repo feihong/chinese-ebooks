172da349caeb6ad8581496a3871c77829895ecbb
Feihong Hsu <feihong.hsu@gmail.com>
2017-12-16 14:15:15 -0600

    Refactor to take advantage of changes to make_ebook()

--- scripts/ba_shijian_dangzuo_pengyou.py
+++ scripts/ba_shijian_dangzuo_pengyou.py
@@ -1,10 +1,12 @@
 from shu import KanunuMultiVolumeScraper, make_ebook
 
 
-class MyScraper(KanunuMultiVolumeScraper):
-    index_url = 'http://www.kanunu8.com/book3/8064/index.html'
-    title = '把时间当作朋友'
-    author = '李笑来'
+# class MyScraper(KanunuMultiVolumeScraper):
 
 
-make_ebook(MyScraper, output_file='books/ba shijian dangzuo pengyou.txt')
+make_ebook(
+    KanunuMultiVolumeScraper,
+    index_url = 'http://www.kanunu8.com/book3/8064/index.html',
+    title = '把时间当作朋友',
+    author = '李笑来',
+    output_file='books/ba shijian dangzuo pengyou')
--- scripts/mingchao_na_xie_shi.py
+++ scripts/mingchao_na_xie_shi.py
@@ -2,27 +2,26 @@ from pathlib import Path
 from shu.kanunu import make_ebook
 
 
-urls = """\
+urls = """
 http://www.kanunu8.com/files/chinese/201102/1777.html
 http://www.kanunu8.com/files/chinese/201102/1778.html
 http://www.kanunu8.com/files/chinese/201102/1766.html
 http://www.kanunu8.com/files/chinese/201102/1779.html
 http://www.kanunu8.com/files/chinese/201102/1780.html
 http://www.kanunu8.com/files/chinese/201102/1767.html
-http://www.kanunu8.com/files/chinese/201102/1781.html""".splitlines()
+http://www.kanunu8.com/files/chinese/201102/1781.html
+""".strip().splitlines()
 
 
 def make(index_url, number):
-    output_file = Path(f'books/mingchao na xie shi {number}.txt')
+    output_file = f'books/mingchao na xie shi {number}'
     print(output_file)
-    # Only make the ebook if it doesn't already exist.
-    # if not output_file.exists():
     make_ebook(
         index_url=index_url,
         title=f'明朝那些事儿{number}',
         author='当年明月',
         output_file=output_file,
-        formats=['.mobi'],
+        formats=['.txt', '.mobi'],
     )
 
 
