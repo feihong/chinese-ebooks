e31635afb33197bfd2b200e75f204c67ce63929e
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-28 21:43:55 -0600

    Site is generated using jinja2 instead of airium

--- tianlongbabu/generate_site.py
+++ tianlongbabu/generate_site.py
@@ -1,63 +1,19 @@
-
-from airium import Airium
 import util
 
 
-def generate_page(title, body_callback, output_file):
-  a = Airium()
-
-  a('<!DOCTYPE html>')
-  with a.html(lang="en"):
-    with a.head():
-        a.meta(charset="utf-8")
-        a.meta(name="viewport", content="width=device-width, initial-scale=1")
-        a.title(_t=title)
-        a.link(rel='stylesheet', href='https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic')
-        a.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css')
-        a.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css')
-        a.style(_t="""
-        p { font-size: 2rem; }
-        span { font-size: 1rem; color: #888; }
-        body { padding: 0 1rem; }
-        """)
-
-    with a.body():
-      a.h1(_t=title)
-      body_callback(a)
-
-    output_file.write_bytes(bytes(a))
-
 def generate_chapter(chapter):
-  title = chapter['title']
+  template = util.get_template('site-chapter.html')
+  output = template.render(title=chapter['title'], body_lines=chapter['body'].splitlines())
+  output_file=(util.output_dir / chapter['title']).with_suffix('.html')
+  output_file.write_text(output)
 
-  def callback(a):
-    lines = chapter['body'].splitlines()
-    for i, line in enumerate(lines, 1):
-      with a.p():
-        a.span(_t=i)
-        a(line)
-
-  generate_page(
-    title=title,
-    body_callback=callback,
-    output_file=(util.output_dir / title).with_suffix('.html'),
-  )
 
 def generate_index(chapters):
-  def callback(a):
-    with a.ol():
-      for chapter in chapters:
-        with a.li():
-          title = chapter['title']
-          link = title + '.html'
-          a.a(href=link, _t=title)
-
-  generate_page(
-    title=util.title,
-    body_callback=callback,
-    output_file=util.output_dir / 'index.html',
-  )
-
+  template = util.get_template('site-index.html')
+  output = template.render(title=util.title, chapters=chapters)
+  output_file = util.output_dir / 'index.html'
+  output_file.write_text(output)
+  
 
 if __name__ == '__main__':
   chapters = util.get_chapters()
--- tianlongbabu/util.py
+++ tianlongbabu/util.py
@@ -1,7 +1,7 @@
 import json
 import re
 from pathlib import Path
-
+import jinja2
 
 here = Path(__file__).parent
 
@@ -19,6 +19,13 @@ output_dir = here / 'output'
 
 hanzi_file = here.parent / 'hanzi.txt'
 ignore_file = here.parent / 'ignore.txt'
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(here / 'templates'),
+    autoescape=jinja2.select_autoescape())
+
+def get_template(template_name):
+  return env.get_template(template_name)
 
 def get_chapters():
   with json_file.open() as fp:
